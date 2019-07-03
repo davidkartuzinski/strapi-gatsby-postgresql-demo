e70f0c71b418ccf26faf36635752a969e3c376c9
davidkartuzinski <davidk@kaidawei.com>
2019-07-03 15:54:08 +0200

    edited apiURL

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,8 +17,8 @@ module.exports = {
       resolve: "gatsby-source-strapi",
       options: {
         apiURL: process.env.DEPLOY_URL
-        ? "https://strapi-gatsby-postgresql-dem.netlify.com"
-        : "http://localhost:1337",
+          ? "https://strapi-gatsby-postgresql-demo.herokuapp.com"
+          : "http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "article",
